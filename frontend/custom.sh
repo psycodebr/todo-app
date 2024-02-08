d1712b9b9d89046911124acfa5a122f9dc2d7ddf
psycodebr <viniciussboscolo@gmail.com>
2024-02-08 16:53:51 -0300

    configurando o pipeline de CI/CD

--- frontend/custom.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Define o diretório onde a aplicação frontend está localizada
-frontend_dir="/app/frontend"
-
-# Executa o build da aplicação frontend, especificando o diretório de saída
-npm run build -- --output-path="$frontend_dir/build" --public-url="/frontend/"
