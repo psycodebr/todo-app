7b35e09d1d185fa0411cf6a1dd943c4664fef8ba
psycodebr <viniciussboscolo@gmail.com>
2024-02-08 01:21:58 -0300

    commit teste 3

--- frontend/index.js
+++ frontend/index.js
@@ -1,11 +1,23 @@
-window.onload = async () => {
-    const response = await fetch('/api/todo');
+// Importar o módulo 'node-fetch' para realizar solicitações HTTP em um ambiente Node.js
+const fetch = require('node-fetch');
+
+// Função assíncrona para buscar e exibir a lista de tarefas
+async function fetchAndDisplayTodoList() {
+  try {
+    // Realizar uma solicitação HTTP GET para /api/todo
+    const response = await fetch('http://localhost:5000/api/todo');
+    // Converter a resposta em JSON
     const todos = await response.json();
-  
-    const todoList = document.getElementById('todo-list');
+
+    // Exibir a lista de tarefas no console
+    console.log('Lista de Tarefas:');
     todos.forEach(todo => {
-      const li = document.createElement('li');
-      li.textContent = todo.task;
-      todoList.appendChild(li);
+      console.log(todo.task);
     });
-  };  
+  } catch (error) {
+    console.error('Ocorreu um erro ao buscar a lista de tarefas:', error);
+  }
+}
+
+// Chamar a função fetchAndDisplayTodoList para buscar e exibir a lista de tarefas
+fetchAndDisplayTodoList();
